1ee5359e449068ed88465b3c36f1caa6d61e7e2f
韩大侠 <347947356@qq.com>
2018-05-27 16:04:47 +0800

    Add files via upload

---
+++ 03_js_01/02_index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2017-10-07 15:08:18
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-10-07 15:18:04
+*/
+
+'use strict';
+	document.write("<font color='red'>打印输出</font><br>");
+	document.write("<span class='txt'>你好，青春!</span><br>");
